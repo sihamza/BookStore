456b44702452ee51274f31435dfff7e9eb06ee25
sihamza <hamzus007jouini@gmail.com>
2019-11-21 21:57:38 +0100

    init project with error and auth page + MD module , routing module , bootstrap cdn

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component' ;
+import { ErrorComponent } from './error/error.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent } ,
+  { path: '**', component: ErrorComponent } 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    )
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule  } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+  AppComponent,
+  LoginComponent,
+  ErrorComponent],
   imports: [
-    BrowserModule
+    FormsModule ,
+    BrowserModule,
+    AppRoutingModule ,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
