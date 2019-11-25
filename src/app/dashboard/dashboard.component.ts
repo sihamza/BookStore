33081b6ad368d1fc639c3a291a2b7c53807e56da
sihamza <hamzus007jouini@gmail.com>
2019-11-25 22:58:08 +0100

    show books in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AjouterComponent } from '../ajouter/ajouter.component';
+import { GbooksService } from "../gbooks.service" ;
 
 @Component({
   selector: 'app-dashboard',
@@ -11,13 +12,16 @@ export class DashboardComponent implements OnInit {
 
   username : String = 'Chocofoxy' ;
   date : Date = new Date() ;
+  stored_books : array = [] ;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog , private books : GbooksService ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.stored_books = await this.books.getBooks('harry potter') ;
   }
 
   openDialog() {
     this.dialog.open(AjouterComponent, { panelClass: 'custom-dialog-container' }); }
+
 
 }
